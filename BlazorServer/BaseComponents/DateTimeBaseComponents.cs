21b60d97fc173c8f2a03a4ee9ea2b24a677330d6
zloisupport <kr0t77@yandex.ru>
2021-12-20 23:03:32 +0600

    Sales DateTime Components

---
+++ BlazorServer/BaseComponents/DateTimeBaseComponents.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.BaseComponents
+{
+    public class DateTimeBaseComponents:ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
