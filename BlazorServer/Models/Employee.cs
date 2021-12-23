2a3114ee351a4738d656fba413f80e3e7d763bd5
zloisupport <kr0t77@yandex.ru>
2021-12-23 13:42:35 +0600

    Lifecycle Events -Avoiding a Data Initiallization Pitfall

---
+++ BlazorServer/Models/Employee.cs
@@ -0,0 +1,17 @@
+namespace BlazorApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+
+        public void LoadRecord()
+        {
+
+        }
+
+    }
+
+}
